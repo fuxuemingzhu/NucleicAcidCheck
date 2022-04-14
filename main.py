601f3bbf0acc050c25251d3c382bcb044603e3fc
负雪明烛 <fuxuemingzhu@users.noreply.github.com>
2022-04-14 09:16:35 +0800

    不用保存图片

--- main.py
+++ main.py
@@ -4,12 +4,9 @@ from paddleocr import PPStructure,draw_structure_result,save_structure_res
 
 table_engine = PPStructure(show_log=True)
 
-save_folder = './output/table'
 img_path = '2022.4.14/张三.JPG'
 img = cv2.imread(img_path)
-
 results = table_engine(img)
-save_structure_res(results, save_folder,os.path.basename(img_path).split('.')[0])
 
 for result in results:
     result.pop('img')
