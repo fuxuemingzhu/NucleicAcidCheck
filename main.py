acaf7685b45e0513ea80fe5ddc7bac660621fa07
负雪明烛 <fuxuemingzhu@users.noreply.github.com>
2022-04-14 09:15:26 +0800

    打印结果

--- main.py
+++ main.py
@@ -7,17 +7,11 @@ table_engine = PPStructure(show_log=True)
 save_folder = './output/table'
 img_path = '2022.4.14/张三.JPG'
 img = cv2.imread(img_path)
-result = table_engine(img)
-save_structure_res(result, save_folder,os.path.basename(img_path).split('.')[0])
 
-for line in result:
-    line.pop('img')
-    print(line)
+results = table_engine(img)
+save_structure_res(results, save_folder,os.path.basename(img_path).split('.')[0])
 
-from PIL import Image
-
-font_path = '../doc/fonts/simfang.ttf' # PaddleOCR下提供字体包
-image = Image.open(img_path).convert('RGB')
-im_show = draw_structure_result(image, result,font_path=font_path)
-im_show = Image.fromarray(im_show)
-im_show.save('result.jpg')
+for result in results:
+    result.pop('img')
+    for line in result["res"]:
+        print(line["text"])
